f3a658986925335ccfe5f9a1a586740c98182102
AlanDuran <alan.duran@iteso.mx>
2023-10-18 22:28:53 -0600

    EWC changes

--- proyecto/ewc_fine_tuner.py
+++ proyecto/ewc_fine_tuner.py
@@ -2,9 +2,11 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torchvision import models
 from torch.utils.data import DataLoader
+from torchvision import models
+from torchvision.models import ResNet50_Weights
 from tqdm import tqdm
+from sklearn.metrics import adjusted_rand_score, normalized_mutual_info_score
 
 
 class EWCFineTuner:
@@ -20,6 +22,7 @@ class EWCFineTuner:
                                    momentum=momentum)
         self.fisher_dict = {}
         self.optpar_dict = {}
+        self.trained_tasks = []
 
     def initialize_model(self, model_name='resnet50'):
         if model_name == 'resnet50':
@@ -27,15 +30,16 @@ class EWCFineTuner:
         elif model_name == 'customNet':
             return Net(3, self.num_classes)
 
-    def train_with_ewc(self, dataloader, num_epochs, ewc_lambda, task):
+    def train_with_ewc(self, dataloader, num_epochs, ewc_lambda):
         self.model.train()
+        task_type = dataloader.dataset.info['task']
 
         for epoch in range(num_epochs):
             for inputs, targets in tqdm(dataloader):
                 self.optimizer.zero_grad()
                 outputs = self.model(inputs.to(self.device))
 
-                if task == 'multi-label, binary-class':
+                if task_type == 'multi-label, binary-class':
                     targets = targets.to(torch.float32)
                     criterion = nn.BCEWithLogitsLoss()
                 else:
@@ -48,19 +52,25 @@ class EWCFineTuner:
                 loss.backward()
                 self.optimizer.step()
 
-        # Update regularization
+        task_id = dataloader.dataset.flag
+        self.optpar_dict[task_id] = {}
+        self.fisher_dict[task_id] = {}
+        self.trained_tasks.append(task_id)
+
+        # Update regularization params
         for name, param in self.model.named_parameters():
-            self.optpar_dict[name] = param.data.clone()
-            self.fisher_dict[name] = param.grad.data.clone().pow(2)
+            self.optpar_dict[task_id][name] = param.data.clone()
+            self.fisher_dict[task_id][name] = param.grad.data.clone().pow(2)
 
     def calculate_ewc_loss(self, ewc_lambda):
         ewc_loss = 0
 
-        for name, param in self.model.named_parameters():
-            if name in self.fisher_dict:
-                fisher = self.fisher_dict[name]
-                optpar = self.optpar_dict[name]
-                ewc_loss += (fisher * (optpar - param).pow(2)).sum() * ewc_lambda
+        for id in self.trained_tasks:
+            for name, param in self.model.named_parameters():
+                fisher = self.fisher_dict[id][name]
+                optpar = self.optpar_dict[id][name]
+                ewc_loss += \
+                    (fisher * (optpar - param).pow(2)).sum() * ewc_lambda
 
         return ewc_loss
 
@@ -70,7 +80,7 @@ class EWCFineTuner:
         eval_dataloader = DataLoader(eval_dataset.test_dataset, batch_size=32,
                                      shuffle=True)
         print(f"Before training (rehearsal={rehearsal}):")
-        self.evaluate_model(eval_dataset.task, eval_dataloader)
+        self.evaluate_model(eval_dataloader)
         last_dataset = None
 
         for i, dataset in enumerate(datasets):
@@ -78,48 +88,78 @@ class EWCFineTuner:
 
             # If rehearsal>0, carry over part of the previous dataset
             if rehearsal>0 and last_dataset is not None:
-                imgs = last_dataset.train_dataset.imgs
-                labels = last_dataset.train_dataset.labels
-                n_samples = len(imgs)
-                random_indices = np.random.choice(
-                    n_samples, int(n_samples*rehearsal), replace=False)
-                imgs_subset = imgs[random_indices]
-                labels_subset = labels[random_indices]
-                dataset.train_dataset.imgs = np.concatenate(
-                    (dataset.train_dataset.imgs, imgs_subset), axis=0)
-                dataset.train_dataset.labels = np.concatenate(
-                    (dataset.train_dataset.labels, labels_subset), axis=0)
+                self.add_data_from_previous_task(
+                    dataset, last_dataset, rehearsal)
 
-            dataloader = DataLoader(dataset.train_dataset, batch_size=32, shuffle=True)
-            self.train_with_ewc(dataloader, num_epochs, ewc_lambda, dataset.task)
-            self.evaluate_model(eval_dataset.task, eval_dataloader)
+            dataloader = DataLoader(
+                dataset.train_dataset, batch_size=32, shuffle=True)
+            self.train_with_ewc(dataloader, num_epochs, ewc_lambda)
+            self.evaluate_model(eval_dataloader)
             last_dataset = dataset
 
-    def evaluate_model(self, task, dataloader):
+    def add_data_from_previous_task(self, dataset, last_dataset, rehearsal):
+        imgs = last_dataset.train_dataset.imgs
+        labels = last_dataset.train_dataset.labels
+        n_samples = len(imgs)
+        # Randomly sample a subset of the data
+        random_idx = np.random.choice(
+            n_samples, int(n_samples * rehearsal), replace=False)
+        print(f"imgs before {dataset.train_dataset.imgs.shape}, "
+              f"labels before {dataset.train_dataset.labels.shape}")
+        # Concatenate the subset to your dataset
+        dataset.train_dataset.imgs = np.concatenate(
+            (dataset.train_dataset.imgs, imgs[random_idx]), axis=0)
+        dataset.train_dataset.labels = np.concatenate(
+            (dataset.train_dataset.labels, labels[random_idx]), axis=0)
+        print(
+            f"size prev imgs {imgs.shape}, labels: {labels.shape}, n_samples {n_samples}")
+        print(
+            f"img subset {imgs[random_idx].shape}, labels subset {labels[random_idx].shape}")
+        print(f"imgs {dataset.train_dataset.imgs.shape}, "
+              f"labels {dataset.train_dataset.labels.shape}")
+
+    def evaluate_model(self, dataloader):
         self.model.eval()
         correct = 0
         total = 0
+        predicted_all = []
+        task_type = dataloader.dataset.info['task']
+
         with torch.no_grad():
             for inputs, labels in tqdm(dataloader):
 
-                if task == 'multi-label, binary-class':
+                if task_type == 'multi-label, binary-class':
                     labels = labels.to(torch.float32)
                 else:
                     labels = labels.squeeze().long()
 
                 outputs = self.model(inputs)
                 _, predicted = torch.max(outputs, 1)
+
                 total += labels.size(0)
+                predicted_all.append(predicted)
                 correct += (predicted == labels).sum().item()
+
+        if task_type == 'multi-label, binary-class':
+            labels = dataloader.dataset.labels
+        else:
+            labels = dataloader.dataset.labels.squeeze()
+
         accuracy = correct / total
+        predicted_all = np.concatenate(predicted_all)
+        ari = adjusted_rand_score(labels, predicted_all)
+        nmi = normalized_mutual_info_score(labels, predicted_all)
+
         print(f"Accuracy on {dataloader.dataset.flag}: {correct}/{total} = {accuracy}")
+        print(f"ARI on {dataloader.dataset.flag}: {ari}")
+        print(f"NMI on {dataloader.dataset.flag}: {nmi}")
         return accuracy
 
     def save_model(self, filename):
         torch.save(self.model.state_dict(), filename)
 
     def get_resnet_model(self):
-        model = models.resnet50(weights=False)
+        model = models.resnet50(weights=ResNet50_Weights.IMAGENET1K_V2)
         # Remove the final classification layer
         # (usually with 1000 classes for ImageNet)
         num_features = model.fc.in_features
--- proyecto/train_medmnist.py
+++ proyecto/train_medmnist.py
@@ -1,33 +1,50 @@
 import random
 
+import numpy as np
+
 from proyecto.ewc_fine_tuner import EWCFineTuner
 from proyecto.medmnist_data import MedMnistData
 
 if __name__ == "__main__":
 
     num_classes = 6
-    learning_rate = 0.01
+    learning_rate = 0.001
     momentum = 0.9
-    num_epochs = 1
-    ewc_lambda = 0.4
-
-    # Initialize the class
-    fine_tuner = EWCFineTuner(num_classes, learning_rate, momentum,
-                              'customNet')
+    num_epochs = 3
+    ewc_lambda = 0
+    rehearsal = 0
+    base_model = 'resnet50'
+    preprocess_resnet = True
 
     # Load and preprocess your datasets
     dataset_names = ['PathMNIST', 'DermaMNIST', 'BloodMNIST', 'TissueMNIST',
                      'OrganCMNIST', 'OrganAMNIST', 'OrganSMNIST']
-    random.shuffle(dataset_names)
+    random.Random(42).shuffle(dataset_names)
     datasets = []
+    smaller_size = 1e6
     for name in dataset_names:
         dataset = MedMnistData(name.lower())
         dataset.select_n_classes(num_classes)
+        dataset_size = len(dataset.train_dataset.imgs)
+        smaller_size = dataset_size if dataset_size < smaller_size else smaller_size
         datasets.append(dataset)
+
+    for dataset in datasets:
+        imgs = dataset.train_dataset.imgs
+        labels = dataset.train_dataset.labels
+        n_samples = len(imgs)
+        # Randomly sample a subset of the data
+        random_idx = np.random.choice(n_samples, smaller_size, replace=False)
+        # Concatenate the subset to your dataset
+        dataset.train_dataset.imgs = imgs[random_idx]
+        dataset.train_dataset.labels = labels[random_idx]
+
+    # Initialize the class
+    fine_tuner = EWCFineTuner(num_classes, learning_rate, momentum, base_model)
 
     # Train on the datasets with EWC
     fine_tuner.train_on_datasets(datasets=datasets[:-1],
                                  num_epochs=num_epochs,
                                  ewc_lambda=ewc_lambda,
                                  eval_dataset=datasets[-1],
-                                 rehearsal=0.2)
+                                 rehearsal=rehearsal)
